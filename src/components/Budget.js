5c9ccf2c2e329bc72ae6d176faeddf3c27927f1f
navesm <stevennave@gmail.com>
2023-05-24 17:58:01 -0400

    made budget value editable

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,22 @@
 import React, {useContext} from 'react';
 import {AppContext} from '../context/AppContext';
 
+
+
 const Budget = () => {
     const {budget} = useContext(AppContext);
     return (
         <div className= 'alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input 
+                  placeholder={budget}
+                  type='number'
+                  style={{size:10}}
+                  step='10'
+                  max="20000"
+                >
+                </input>
+            </span>
         </div>
     );
 };
